aa3fa46b0935078a2244122080ac39409625b662
Stellafreedberg <stellafreedberg@ucsb.edu>
2025-03-15 20:22:53 -0700

    editiedcomm101

--- exercises/comm101c.R
+++ exercises/comm101c.R
@@ -6,6 +6,7 @@ library(ggplot2)
 # 2) Clean up the axis labels
 # 3) Make the color vary by island
 # 4) Choose a color palette that you enjoy!
+okabe_ito<- c("#E69F00","#56B4E9","#009573", "#F0E442", "#0072E2")
 ggplot(penguins, aes(x=island, y=body_mass_g, color=island)) +
   geom_boxplot() +
   scale_color_manual(values=okabe_ito) +
